420ad89da70339e8496453840521f8cae6a07a31
Your Name <you@example.com>
2023-10-19 13:56:58 +0200

    working on hover icons

--- src/components/child components/CoverImage.jsx
+++ src/components/child components/CoverImage.jsx
@@ -1,15 +1,42 @@
+import {useState} from 'react';
+
+function Hover() {
+    return (
+        <span className="icon-container">
+            <button className="icon heart"></button>
+            <button className="icon play"></button>
+            <button className="icon dots"></button>
+        </span>
+    );
+  }
+
 
 export const CoverImage = ({ images, albumUrl }) => {
-    
     const selectedImage = images.filter(
         ({height}) => height === 300
     )
     const selectedUrl = selectedImage[0].url
     //console.log(selectedImage);
+
+    
+    const [isHovering, setIsHovering] = useState(false);
+    const handleMouseOver = () => {
+      setIsHovering(true);
+    };
+    const handleMouseOut = () => {
+      setIsHovering(false);
+    };
+
+
     return (
-        <div className="albumCover">
-           <a href={albumUrl}><img src={selectedUrl} alt="Album cover"/></a>
+        <div className="albumCover" onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}>
+            
+           <a href={albumUrl}><img className="darker" src={selectedUrl} alt="Album cover"/></a>
+
+           {isHovering && <Hover />}
         </div>
+        
     );
 };
 
