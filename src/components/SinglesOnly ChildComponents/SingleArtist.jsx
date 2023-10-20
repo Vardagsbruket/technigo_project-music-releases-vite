e0a56f0f0069c65cc95f8046963e1c910bf7ce43
Your Name <you@example.com>
2023-10-20 22:55:46 +0200

    Final draft

--- src/components/SinglesOnly ChildComponents/SingleArtist.jsx
+++ src/components/SinglesOnly ChildComponents/SingleArtist.jsx
@@ -1,23 +1,22 @@
 export const SingleArtist = ({ artists, album_type, release_date, total_tracks }) => {
     
-        const selectArtist = album_type === "single" ? artists : [];
-      
-        const artistLinks = selectArtist.map((artist, index) => (
-            <span key={index}>
-                <a href={artist.external_urls.spotify}>
-                    {artist.name}
-                </a>
-                {index < selectArtist.length - 1 && ', '}
-            </span>
-          ));
-      
-        return (
-          <div className="singleArtist">
-            <p>{artistLinks}</p>
-            <p>Release date: {release_date}</p>
-            <p>Total tracks: {total_tracks}</p>
-          </div>
-        );
-      };
-      
-    
+    const selectArtist = album_type === "single" ? artists : [];
+  
+    const artistLinks = selectArtist.map((artist, index) => (
+        <span key={index}>
+            <a href={artist.external_urls.spotify}>
+                {artist.name}
+            </a>
+            {index < selectArtist.length - 1 && ', '}
+        </span>
+      ));
+  
+    return (
+      <div className="singleArtist">
+        <p>{artistLinks}</p>
+        <p>Release date: {release_date}</p>
+        <p>Total tracks: {total_tracks}</p>
+      </div>
+    );
+  };
+  
--- src/components/SinglesOnly ChildComponents/SingleName.jsx
+++ src/components/SinglesOnly ChildComponents/SingleName.jsx
@@ -1,10 +1,14 @@
-export const SingleName = ({name, album_type, albumUrl}) => {
-    const selectSingle = name
-    if (album_type === "single")
-        
-    return (
+export const SingleName = ({ name, album_type, albumUrl }) => {
+    const selectSingle = name;
+  
+    if (album_type === "single") 
+      return (
         <div className="singleName">
-            <a href={albumUrl}><p>{selectSingle}</p></a>
+          <a href={albumUrl}>
+            <p>{selectSingle}</p>
+          </a>
         </div>
-    )
-}
+      );
+    
+  };
+  
