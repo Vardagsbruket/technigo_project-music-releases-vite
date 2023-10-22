2e7181b303221870964c29909b90f3e833b96434
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-10-22 10:53:16 +0200

    Diana add final css + fix bug with artist name separation (#12)
    * start on project structure
    
    * complete project folder structure + render album info
    
    * add album url + responsiveness
    
    * add playlist feature
    
    * add Singles Only feature
    
    * add fix feature singles only
    
    * add Albums Only feature
    
    * add nav bar + styling
    
    * fix bug after merge
    
    * comment out unused function
    
    * fix typo
    
    * remove broken file and replace with PlaylistsNew
    
    * add styles to media screens desktop and tablet
    
    * fix empty gridblocks in array + bug in SinglesArtists
    
    * add final css and fix bug with artist names

--- src/components/AlbumOnly ChildComponents/AlbumsArtist.jsx
+++ src/components/AlbumOnly ChildComponents/AlbumsArtist.jsx
@@ -1,23 +1,21 @@
-export const AlbumsArtist = ({artists, release_date, total_tracks}) => {
+export const AlbumsArtist = ({ artists, album_type, release_date, total_tracks }) => {
+  const selectArtist = album_type === "album" ? artists : [];
+  const artistLinks = selectArtist.map((artist, index) => (
+      <span key={index}>
+        <a href={artist.external_urls.spotify}>
+        {artist.name}
+        </a>
+        {index < selectArtist.length - 1 && ', '}
+      </span>
+    ));
+  return (
+    <div className="albumArtist">
+      <p>{artistLinks}</p>
+      <p className="extraInfo">Release date: {release_date}</p>
+      <p className="extraInfo">Total tracks: {total_tracks}</p>
+    </div>
+  );
+};
 
-    //const selectArtist = album_type === "album" ? artists : [];
-   
-    
-    const artistLinks = artists.map((artist, index) => (
-        <span key={index}>
-            <a href={artist.external_urls.spotify}>
-                {artist.name}
-            </a>
-            {index < artist.length - 1 && ', '}
-        </span>
-      ));
-  
-    return (
-      <div className="albumArtist">
-        <p>{artistLinks}</p>
-        <p>Release date: {release_date}</p>
-        <p>Total tracks: {total_tracks}</p>
-      </div>
-    );
-  };
+
 
--- src/components/SinglesOnly ChildComponents/SingleArtist.jsx
+++ src/components/SinglesOnly ChildComponents/SingleArtist.jsx
@@ -1,35 +1,18 @@
-export const SingleArtist = ({artists, release_date, total_tracks}) => {
-
-    //const selectArtist = album_type === "single" ? artists : [];
-   console.log(release_date, total_tracks)
-
-    //const artistNames = selectArtist.map(({name}) => name).join(", ");
-    
-    //const artistUrl = artists[0].external_urls.spotify;
-
-
-    const artistLinks = artists.map((artist, index) => (
-        <span key={index}>
-            <a href={artist.external_urls.spotify}>
-                {artist.name}
-            </a>
-            {index < artists.length - 1 && ', '}
-        </span>
-      ));
-  
-    return (
-      <div className="singleArtist">
-        <p>{artistLinks}</p>
-        <p>Release date: {release_date}</p>
-        <p>Total tracks: {total_tracks}</p>
-      </div>
-    );
-  };
-
-
-//const renderArtistNames = () => artists.map(
-    //({name}) => name + ", "
-    //);
-
-    // const selectArtist = artists
-    //if (album_type === "single")
+export const SingleArtist = ({ artists, album_type, release_date, total_tracks }) => {
+  const selectArtist = album_type === "single" ? artists : [];
+  const artistLinks = selectArtist.map((artist, index) => (
+      <span key={index}>
+        <a href={artist.external_urls.spotify}>
+          {artist.name}
+        </a>
+        {index < selectArtist.length - 1 && ', '}
+      </span>
+    ));
+  return (
+    <div className="singleArtist">
+      <p>{artistLinks}</p>
+      <p className="extraInfo">Release date: {release_date}</p>
+      <p className="extraInfo">Total tracks: {total_tracks}</p>
+    </div>
+  );
+};
